abf31ed1e4a4465bab97f2fcef18af182ccf02f4
Thomas Logan <thomaslilogan@gmail.com>
2017-09-12 12:08:29 -0400

    update syntax

--- Communication_Topology.thy
+++ Communication_Topology.thy
@@ -30,41 +30,41 @@ and bind =
 | B_Prim prim
 | B_App var var
     
-definition exp_let :: "string \<Rightarrow> bind \<Rightarrow> exp \<Rightarrow> exp" ("LET _ BE _ IN _" [0,0, 61] 61) where
-  "LET x BE b IN e = E_Let (Var x) b e"
+abbreviation exp_let :: "string \<Rightarrow> bind \<Rightarrow> exp \<Rightarrow> exp" ("LET _ = _ |> _" [0,0, 61] 61) where
+  "LET x = b |> e \<equiv> E_Let (Var x) b e"
   
-definition exp_var :: "string \<Rightarrow> exp" ("VAR _" [61] 61) where
-  "VAR x = E_Var (Var x)"
+abbreviation exp_var :: "string \<Rightarrow> exp" ("VAR _" [61] 61) where
+  "VAR x \<equiv> E_Var (Var x)"
 
-definition bind_send :: "string \<Rightarrow> string \<Rightarrow> bind" ("SEND _ _" [61, 61] 61) where
-  "SEND x y = B_Send (Var x) (Var y)"
+abbreviation bind_send :: "string \<Rightarrow> string \<Rightarrow> bind" ("SEND _ _" [61, 61] 61) where
+  "SEND x y \<equiv> B_Send (Var x) (Var y)"
   
-definition bind_recv :: "string \<Rightarrow> bind" ("RECV _" [61] 61) where
-  "RECV x = B_Recv (Var x)"
+abbreviation bind_recv :: "string \<Rightarrow> bind" ("RECV _" [61] 61) where
+  "RECV x \<equiv> B_Recv (Var x)"
   
-definition bind_fst :: "string \<Rightarrow> bind" ("FST _" [61] 61) where
-  "FST x = B_Fst (Var x)"
+abbreviation bind_fst :: "string \<Rightarrow> bind" ("FST _" [61] 61) where
+  "FST x \<equiv> B_Fst (Var x)"
   
-definition bind_snd :: "string \<Rightarrow> bind" ("SND _" [61] 61) where
-  "SND x = B_Snd (Var x)"
+abbreviation bind_snd :: "string \<Rightarrow> bind" ("SND _" [61] 61) where
+  "SND x \<equiv> B_Snd (Var x)"
   
-definition bind_app :: "string \<Rightarrow> string \<Rightarrow> bind" ("APP _ _" [61, 61] 61) where
-  "APP x y = B_App (Var x) (Var y)"
+abbreviation bind_app :: "string \<Rightarrow> string \<Rightarrow> bind" ("APP _ _" [61, 61] 61) where
+  "APP x y \<equiv> B_App (Var x) (Var y)"
 
-definition bind_case :: "string \<Rightarrow> string \<Rightarrow> exp \<Rightarrow> string \<Rightarrow> exp \<Rightarrow> bind" ("CASE _ LEFT _ IN _ RIGHT _ IN _" [0,0,0,0, 61] 61) where
-  "CASE x LEFT xl IN el RIGHT xr IN er = B_Prim (PT_Case (Var x) (Var xl) el (Var xr) er)"
+abbreviation bind_case :: "string \<Rightarrow> string \<Rightarrow> exp \<Rightarrow> string \<Rightarrow> exp \<Rightarrow> bind" ("CASE _ LEFT _ |> _ RIGHT _ |> _" [0,0,0,0, 61] 61) where
+  "CASE x LEFT xl |> el RIGHT xr |> er \<equiv> B_Prim (PT_Case (Var x) (Var xl) el (Var xr) er)"
   
-definition bind_abs :: "string => string => exp => bind" ("FUN _, _ IN _" [0, 0, 61] 61) where
-  "FUN f, x IN e = B_Prim (PT_Abs (Var f) (Var x) e)"
+abbreviation bind_abs :: "string => string => exp => bind" ("FUN _, _ |> _" [0, 0, 61] 61) where
+  "FUN f, x |> e \<equiv> B_Prim (PT_Abs (Var f) (Var x) e)"
   
-definition bind_pair :: "string => string => bind" ("\<guillemotleft>_, _\<guillemotright>" [0, 0] 61) where
-  "\<guillemotleft>x, y\<guillemotright> = B_Prim (PT_Pair (Var x) (Var y))"
+abbreviation bind_pair :: "string => string => bind" ("\<lparr>_, _\<rparr>" [0, 0] 61) where
+  "\<lparr>x, y\<rparr> \<equiv> B_Prim (PT_Pair (Var x) (Var y))"
   
-definition bind_inl :: "string => bind" ("LEFT _" [61] 61) where
-  "LEFT x = B_Prim (PT_Left (Var x))"
+abbreviation bind_inl :: "string => bind" ("LEFT _" [61] 61) where
+  "LEFT x \<equiv> B_Prim (PT_Left (Var x))"
   
-definition bind_inr :: "string => bind" ("RIGHT _" [61] 61) where
-  "RIGHT x = B_Prim (PT_Right (Var x))"
+abbreviation bind_inr :: "string => bind" ("RIGHT _" [61] 61) where
+  "RIGHT x \<equiv> B_Prim (PT_Right (Var x))"
   
 value "Map.empty (Var ''x'' \<mapsto> 4)"
   
@@ -75,7 +75,22 @@ datatype val =
 | V_Unit
   
   
-value "LET (Var ''y'') BE UNIT IN E_Var (Var ''y'')"
+value "LET ''y'' = \<lparr>\<rparr> |> VAR ''y''"
+
+value "
+LET ''y'' = \<lparr>\<rparr> |>
+LET ''x'' = CHAN \<lparr>\<rparr> |>
+LET ''z'' = \<lparr>''x'', ''y''\<rparr> |>
+LET ''a'' = SEND ''x'' ''y'' |>
+LET ''b'' = FST ''z'' |>
+LET ''c'' = RIGHT ''b'' |>
+LET ''d'' = 
+  CASE ''c''
+  LEFT ''cl'' |> VAR ''cl''
+  RIGHT ''cr'' |> VAR ''cr''
+|>
+VAR ''z''
+"
 
 datatype cont = Cont var exp "var \<rightharpoonup> val" ("\<langle>_,_,_\<rangle>" [0, 0, 0] 70) 
 
