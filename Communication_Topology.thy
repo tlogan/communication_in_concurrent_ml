447d16d8a3a298226af3116ffb6dccf08cb165e3
Thomas Logan <thomaslilogan@gmail.com>
2017-09-22 12:53:11 -0400

    update

--- Communication_Topology.thy
+++ Communication_Topology.thy
@@ -6,7 +6,8 @@ datatype var = Var string
   
 value " Var ''x''"
   
-datatype chan = Ch "var list"
+type_synonym control_path = "(var + unit) list"
+datatype chan = Ch control_path
   
 datatype exp = 
   E_Let var bind exp ("LET _ = _ in _" [0,0, 61] 61) |
@@ -66,6 +67,9 @@ datatype cont = Cont var exp "var \<rightharpoonup> val" ("\<langle>_,_,_\<rangl
 
 type_synonym state = "exp \<times> (var \<rightharpoonup> val) \<times> cont list"
   
+type_synonym val_pool = "control_path \<rightharpoonup> val"
+type_synonym state_pool = "control_path \<rightharpoonup> state"
+  
 inductive seq_step :: "state \<Rightarrow> state \<Rightarrow> bool" (infix "\<hookrightarrow>" 55) where 
   Seq_Result: "
     (\<rho> x) = Some \<omega> \<Longrightarrow>
@@ -109,9 +113,8 @@ inductive seq_steps :: "state \<Rightarrow> state \<Rightarrow> bool" (infix "\<
   Seqs_Refl: "x \<hookrightarrow>* x" |
   Seqs_Step: "\<lbrakk>x \<hookrightarrow> y ; y \<hookrightarrow>* z\<rbrakk> \<Longrightarrow> x \<hookrightarrow>* z"
   
-type_synonym control_path = "(var + unit) list"
-type_synonym val_pool = "control_path \<rightharpoonup> val"
-type_synonym state_pool = "control_path \<rightharpoonup> state"
+
+
   
   
 abbreviation control_path_append_var :: "control_path => var => control_path" (infixl ";;" 61) where
@@ -157,7 +160,7 @@ inductive concur_step :: "state_pool \<Rightarrow> state_pool \<Rightarrow> bool
         leaf stpool \<pi> \<longrightarrow>
         (stpool \<pi>) = Some (LET x = SYNC x_evt in e, \<rho>, \<kappa>) \<longrightarrow>
         (vpool \<pi>) = (\<rho> x_evt) \<longrightarrow>
-        (vpool' \<pi>) = Some \<lbrace>P_Awlays_Evt x_a, [x_a \<mapsto> \<omega>_a]\<rbrace> \<longrightarrow>
+        (vpool' \<pi>) = Some \<lbrace>P_Always_Evt x_a, [x_a \<mapsto> \<omega>_a]\<rbrace> \<longrightarrow>
         stpool_sync (\<pi>;;x) = Some (e, \<rho>(x \<mapsto> \<omega>_a), \<kappa>)
       )
     \<rbrakk> \<Longrightarrow>
@@ -176,6 +179,62 @@ inductive concur_step :: "state_pool \<Rightarrow> state_pool \<Rightarrow> bool
       \<pi>;;. \<mapsto> (e_child, \<rho>, \<kappa>) 
     )
   "
+  
+inductive concur_steps :: "state_pool \<Rightarrow> state_pool \<Rightarrow> bool" (infix "\<rightarrow>*" 55) where
+  Concurs_Refl: "x \<rightarrow>* x" |
+  Concurs_Step: "\<lbrakk>x \<rightarrow> y ; y \<rightarrow>* z\<rbrakk> \<Longrightarrow> x \<rightarrow>* z"
+  
+definition send_sites :: "state_pool \<Rightarrow> chan \<Rightarrow> control_path set" where
+  "send_sites stpool ch = {\<pi>. \<exists> x x_evt e \<kappa> \<rho> x_ch x_m \<rho>_evt. 
+    stpool \<pi> = Some (LET x = SYNC x_evt in e, \<rho>, \<kappa>) \<and> 
+    \<rho> x_evt = Some \<lbrace>P_Send_Evt x_ch x_m, \<rho>_evt\<rbrace> \<and> 
+    \<rho>_evt x_ch = Some \<lbrace>ch\<rbrace>
+  }"
+  
+definition recv_sites :: "state_pool \<Rightarrow> chan \<Rightarrow> control_path set" where
+  "recv_sites stpool ch = {\<pi>. \<exists> x x_evt e \<kappa> \<rho> x_ch \<rho>_evt. 
+    stpool \<pi> = Some (LET x = SYNC x_evt in e, \<rho>, \<kappa>) \<and> 
+    \<rho> x_evt = Some \<lbrace>P_Recv_Evt x_ch, \<rho>_evt\<rbrace> \<and> 
+    \<rho>_evt x_ch = Some \<lbrace>ch\<rbrace>
+  }"
+  
+fun channel_exists :: "state_pool \<Rightarrow> chan \<Rightarrow> bool" where
+  "channel_exists stpool (Ch \<pi>) \<longleftrightarrow> (\<exists> x e \<rho> \<kappa>. stpool \<pi> = Some (LET x = CHAN \<lparr>\<rparr> in e, \<rho>, \<kappa>))"
+  
+definition state_pool_possible :: "exp \<Rightarrow> state_pool \<Rightarrow> bool" where
+  "state_pool_possible e stpool \<longleftrightarrow> [[] \<mapsto> (e, Map.empty, [])] \<rightarrow>* stpool"
+  
+definition one_shot :: "exp \<Rightarrow> var \<Rightarrow> bool" where
+  "one_shot e x \<longleftrightarrow> (\<forall> stpool \<pi>. 
+    state_pool_possible e stpool \<longrightarrow>
+    channel_exists stpool (Ch (\<pi>;;x)) \<longrightarrow> (*chan_exists doesn't seem necessary*)
+    card (send_sites stpool (Ch (\<pi>;;x))) \<le> 1
+  )"
+  
+definition single_side :: "(state_pool \<Rightarrow> chan \<Rightarrow> control_path set) \<Rightarrow> exp \<Rightarrow> var \<Rightarrow> bool" where
+  "single_side sites e x \<longleftrightarrow> (\<forall> stpool \<pi> \<pi>_1 \<pi>_2 .
+    state_pool_possible e stpool \<longrightarrow>
+    channel_exists stpool (Ch (\<pi>;;x)) \<longrightarrow>
+    (let site_set = (sites stpool (Ch (\<pi>;;x))) in 
+      \<pi>_1 \<in> site_set \<and> \<pi>_2 \<in> site_set
+    ) \<longrightarrow>
+    (prefix \<pi>_1 \<pi>_2 \<or> prefix \<pi>_2 \<pi>_1) 
+  )"
+  
+definition single_receiver :: "exp \<Rightarrow> var \<Rightarrow> bool" where 
+  "single_receiver = single_side recv_sites"
+  
+definition single_sender :: "exp \<Rightarrow> var \<Rightarrow> bool" where 
+  "single_sender = single_side send_sites"
+  
+definition point_to_point :: "exp \<Rightarrow> var \<Rightarrow> bool" where
+  "point_to_point e x \<longleftrightarrow> single_sender e x \<and> single_receiver e x"
+  
+definition fan_out :: "exp \<Rightarrow> var \<Rightarrow> bool" where
+  "fan_out e x \<longleftrightarrow> single_sender e x \<and> \<not> single_receiver e x "
+  
+definition fan_in :: "exp \<Rightarrow> var \<Rightarrow> bool" where
+  "fan_in e x \<longleftrightarrow> \<not> single_sender e x \<and> single_receiver e x "
   
 abbreviation a where "a \<equiv> Var ''a''"
 abbreviation b where "b \<equiv> Var ''b''"
