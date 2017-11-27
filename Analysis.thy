15f5212f8469b209985b452c3647fadb4053ebc1
Thomas Logan <thomaslilogan@gmail.com>
2017-11-27 11:19:08 -0800

    update

--- Analysis.thy
+++ Analysis.thy
@@ -126,33 +126,125 @@ inductive abstract_value_flow :: "abstract_value_env \<times> abstract_value_env
     (\<V>, \<C>) \<Turnstile> LET x = SPAWN e\<^sub>c in e
   "
 
-fun absval :: "val option \<Rightarrow> abstract_value set" where
-  "absval (Some \<lbrace>Ch \<pi> x\<rbrace>) = {^Chan x}" |
-  "absval (Some \<lbrace>p, \<rho>\<rbrace>) = {^p}" |
-  "absval (Some \<lbrace>\<rbrace>) = {^\<lparr>\<rparr>}" |
-  "absval None = {}"
+fun absval :: "val \<Rightarrow> abstract_value" where
+  "absval \<lbrace>Ch \<pi> x\<rbrace> = ^Chan x" |
+  "absval \<lbrace>p, \<rho>\<rbrace> = ^p" |
+  "absval \<lbrace>\<rbrace> = ^\<lparr>\<rparr>"
 
 definition absval_env :: "(var \<rightharpoonup> val) \<Rightarrow> var \<Rightarrow> abstract_value set" where
-  "absval_env \<rho> x = absval (\<rho> x)"
+  "absval_env \<rho> x = (case (\<rho> x) of 
+    Some \<omega> \<Rightarrow> {absval \<omega>} |
+    None \<Rightarrow> {}
+  )"
 
 
 definition abstract_more_precise :: "abstract_value_env \<Rightarrow> abstract_value_env \<Rightarrow> prop" (infix "\<sqsubseteq>" 55) where
   "abstract_more_precise \<V> \<V>' \<equiv> (\<And> x . \<V> x \<subseteq> \<V>' x)"
 
-inductive abstract_value_flow_pool :: "abstract_value_env \<times> abstract_value_env \<Rightarrow> state_pool \<Rightarrow> bool" (infix "\<Turnstile>\<^sub>p" 55) where
-  Dummy: "E \<Turnstile>\<^sub>p E'"
+inductive abstract_value_flow_value :: "abstract_value_env \<times> abstract_value_env \<Rightarrow> val \<Rightarrow> bool" (infix "\<parallel>>" 55)
+and  abstract_value_flow_env :: "abstract_value_env \<times> abstract_value_env \<Rightarrow> (var \<rightharpoonup> val) \<Rightarrow> bool" (infix "\<parallel>\<approx>" 55) 
+where
+  Unit: "(\<V>, \<C>) \<parallel>> \<lbrace>\<rbrace>" |
+  Chan: "(\<V>, \<C>) \<parallel>> \<lbrace>c\<rbrace>" |
+  Abs: "
+    \<lbrakk>
+      {^Abs f x e} \<subseteq> \<V> f;
+      (\<V>, \<C>) \<Turnstile> e;
+      (\<V>, \<C>) \<parallel>\<approx> \<rho>
+    \<rbrakk> \<Longrightarrow>
+    (\<V>, \<C>) \<parallel>> \<lbrace>Abs f x e, \<rho>\<rbrace>
+  " |
+  Pair: "
+    \<lbrakk>
+      (\<V>, \<C>) \<Turnstile> e;
+      (\<V>, \<C>) \<parallel>\<approx> \<rho>
+    \<rbrakk> \<Longrightarrow>
+    (\<V>, \<C>) \<parallel>> \<lbrace>Pair _ _, \<rho>\<rbrace>
+  " |
+  Left: "
+    \<lbrakk>
+      (\<V>, \<C>) \<Turnstile> e;
+      (\<V>, \<C>) \<parallel>\<approx> \<rho>
+    \<rbrakk> \<Longrightarrow>
+    (\<V>, \<C>) \<parallel>> \<lbrace>Left _, \<rho>\<rbrace>
+  " |
+  Right: "
+    \<lbrakk>
+      (\<V>, \<C>) \<Turnstile> e;
+      (\<V>, \<C>) \<parallel>\<approx> \<rho>
+    \<rbrakk> \<Longrightarrow>
+    (\<V>, \<C>) \<parallel>> \<lbrace>Right _, \<rho>\<rbrace>
+  " |
+  Send_Evt: "
+    \<lbrakk>
+      (\<V>, \<C>) \<Turnstile> e;
+      (\<V>, \<C>) \<parallel>\<approx> \<rho>
+    \<rbrakk> \<Longrightarrow>
+    (\<V>, \<C>) \<parallel>> \<lbrace>Send_Evt _ _, \<rho>\<rbrace>
+  " |
+  Recv_Evt: "
+    \<lbrakk>
+      (\<V>, \<C>) \<Turnstile> e;
+      (\<V>, \<C>) \<parallel>\<approx> \<rho>
+    \<rbrakk> \<Longrightarrow>
+    (\<V>, \<C>) \<parallel>> \<lbrace>Recv_Evt _, \<rho>\<rbrace>
+  " |
+  Always_Evt: "
+    \<lbrakk>
+      (\<V>, \<C>) \<Turnstile> e;
+      (\<V>, \<C>) \<parallel>\<approx> \<rho>
+    \<rbrakk> \<Longrightarrow>
+    (\<V>, \<C>) \<parallel>> \<lbrace>Always_Evt _, \<rho>\<rbrace>
+  " |
+
+  Empty: "(\<V>, \<C>) \<parallel>\<approx> empty" |
+  Nonempty: "
+    \<lbrakk>
+      {absval \<omega>} \<subseteq> \<V> x;
+      (\<V>, \<C>) \<parallel>> \<omega>;
+      (\<V>, \<C>) \<parallel>\<approx> \<rho>
+    \<rbrakk> \<Longrightarrow>
+    (\<V>, \<C>) \<parallel>\<approx> \<rho>(x \<mapsto> \<omega>)
+  "
+
+inductive abstract_value_flow_stack :: "abstract_value_env \<times> abstract_value_env \<Rightarrow> abstract_value set \<Rightarrow> cont list \<Rightarrow> bool" ("_ \<Turnstile> _ \<Rrightarrow> _" [56, 0, 56] 56) where
+  "
+    (\<V>, \<C>) \<Turnstile> \<W> \<Rrightarrow> \<kappa>
+  "
+
+
+  
+  
+
+inductive abstract_value_flow_state :: "abstract_value_env \<times> abstract_value_env \<Rightarrow> state \<Rightarrow> bool" (infix "\<TTurnstile>" 55) where
+  "
+    \<lbrakk>
+      (\<V>, \<C>) \<Turnstile> e; 
+      (\<V>, \<C>) \<parallel>\<approx> \<rho>; 
+      (\<V>, \<C>) \<Turnstile> \<V> (result_var e) \<Rrightarrow> \<kappa>
+    \<rbrakk> \<Longrightarrow>
+    (\<V>, \<C>) \<TTurnstile> (e, \<rho>, \<kappa>)
+  "
+
+inductive abstract_value_flow_pool :: "abstract_value_env \<times> abstract_value_env \<Rightarrow> state_pool \<Rightarrow> bool" (infix "\<parallel>\<lless>" 55) where
+  "
+    \<lbrakk>
+      \<And> \<pi> . \<E> \<pi> = Some \<sigma> \<Longrightarrow> (\<V>, \<C>) \<TTurnstile> \<sigma>
+    \<rbrakk> \<Longrightarrow>
+    (\<V>, \<C>) \<parallel>\<lless> \<E>
+  "
 
 theorem abstract_value_flow_preservation : "
   \<lbrakk>
-    (\<V>, \<C>) \<Turnstile>\<^sub>p \<E>; 
+    (\<V>, \<C>) \<parallel>\<lless> \<E>; 
     \<E> \<rightarrow> \<E>'
   \<rbrakk> \<Longrightarrow>
-  (\<V>, \<C>) \<Turnstile>\<^sub>p \<E>'
+  (\<V>, \<C>) \<parallel>\<lless> \<E>'
 "
 sorry
 
 theorem abstract_value_flow_precision : "
-  (\<V>, \<C>) \<Turnstile>\<^sub>p \<E>  (* or should it be (\<V>, \<C>) \<Turnstile>\<^sub>r \<rho> *)
+  (\<V>, \<C>) \<parallel>\<lless>  \<E>  (* or should it be (\<V>, \<C>) \<parallel>\<approx> \<rho> *)
   \<Longrightarrow>
   (\<And> \<pi> . \<E> \<pi> = Some (e, \<rho>, \<kappa>) \<Longrightarrow>
     absval_env \<rho> \<sqsubseteq> \<V>
@@ -163,7 +255,7 @@ sorry
 theorem abstract_value_flow_sound : "
   \<lbrakk>
     (\<V>, \<C>) \<Turnstile> e; 
-    [[] \<mapsto> (e, empty, [])] \<rightarrow>* \<E>' 
+    [[] \<mapsto> (e, empty, [])] \<rightarrow>* \<E>'
   \<rbrakk> \<Longrightarrow>
   (\<And> \<pi> . \<E>' \<pi> = Some (e', \<rho>', \<kappa>') \<Longrightarrow>
     absval_env \<rho>' \<sqsubseteq> \<V>
