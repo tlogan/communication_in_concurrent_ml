73e0fbad51aa815145860bf695eae1f2cfa35dd6
Thomas Logan <thomaslilogan@gmail.com>
2018-01-21 22:12:14 -0500

    add proofs for equiv between star and star_left

--- Semantics.thy
+++ Semantics.thy
@@ -251,16 +251,27 @@ inductive star_left :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> '
  refl: "star_left R z z" |
  step: "star_left R x y \<Longrightarrow> R y z \<Longrightarrow> star_left R x z"
 
+lemma star_left_trans: "
+  star_left r y z \<Longrightarrow> star_left r x y \<Longrightarrow> star_left r x z
+"
+proof(induction rule: star_left.induct)
+  case refl thus ?case .
+next
+  case step thus ?case by (metis star_left.step)
+qed
 
-lemma "star R x z \<Longrightarrow> star R x y \<longrightarrow> R y z"
-sorry
+
+lemma star_left_step1[simp, intro]: "r x y \<Longrightarrow> star_left r x y"
+by(metis star_left.refl star_left.step)
 
 theorem star_implies_star_left: "star R x z \<Longrightarrow> star_left R x z"
- apply (case_tac "x = z"; auto?; (rule refl)?)
- apply (erule star.cases; auto)
-sorry
+ apply (erule star.induct, rule star_left.refl)
+ apply (meson star_left_step1 star_left_trans)
+done
 
 theorem star_left_implies_star: "star_left R x z \<Longrightarrow> star R x z"
-sorry
+ apply (erule star_left.induct, rule star.refl)
+ apply (meson star_step1 star_trans)
+done
 
 end
