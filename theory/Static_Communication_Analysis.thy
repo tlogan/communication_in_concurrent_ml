2a781dc2a4a9235d6aa85d077893c06ecc973f0d
Thomas Logan <thomaslilogan@gmail.com>
2018-03-14 21:01:19 -0400

    start sketch of flow relation

--- theory/Static_Communication_Analysis.thy
+++ theory/Static_Communication_Analysis.thy
@@ -2,36 +2,149 @@ theory Static_Communication_Analysis
   imports Main Syntax Runtime_Semantics Static_Semantics Runtime_Communication_Analysis
 begin
 
+datatype flow_label = Message | Step
 
-
-datatype topo = Non | OneShot | OneToOne | FanOut | FanIn | ManyToMany
-type_synonym topo_pair = "var \<times> topo"
-type_synonym topo_env = "var \<Rightarrow> topo"
-
-
-definition var_to_topo :: "state_pool \<Rightarrow> control_path \<Rightarrow> var \<Rightarrow> topo" ("\<langle>\<langle>_ ; _ ; _\<rangle>\<rangle>" [0,0,0]61) where
-  "\<langle>\<langle>\<E> ; \<pi>; x\<rangle>\<rangle> \<equiv>
-    (if  one_shot \<E> (Ch \<pi> x) then OneShot
-    else (if one_to_one \<E> (Ch \<pi> x) then OneToOne
-    else (if fan_out \<E> (Ch \<pi> x) then FanOut
-    else (if fan_in \<E> (Ch \<pi> x) then FanIn
-    else ManyToMany))))
+type_synonym flow_set = "(control_label \<times> flow_label \<times> control_label) set"
+inductive flow :: "(abstract_value_env \<times> abstract_value_env \<times> flow_set) \<Rightarrow> exp \<Rightarrow> bool" (infix "\<TTurnstile>" 55) where
+  "(\<V>, \<C>, \<F>) \<TTurnstile> e" 
+(*
+  Result: "
+    (\<V>, \<C>) \<Turnstile>\<^sub>e (RESULT x)
+  " |
+  Let_Unit: "
+    \<lbrakk>
+      {^\<lparr>\<rparr>} \<subseteq> \<V> x;
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e
+    \<rbrakk> \<Longrightarrow> 
+    (\<V>, \<C>) \<Turnstile>\<^sub>e LET x = \<lparr>\<rparr> in e
+  " |
+  Let_Abs : "
+    \<lbrakk>
+      {^Abs f' x' e'} \<subseteq> \<V> f';
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e';
+      {^Abs f' x' e'} \<subseteq> \<V> x;
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e 
+    \<rbrakk> \<Longrightarrow> 
+    (\<V>, \<C>) \<Turnstile>\<^sub>e LET x = FN f' x' . e' in e
+  " |
+  Let_Pair : "
+    \<lbrakk>
+      {^Pair x\<^sub>1 x\<^sub>2} \<subseteq> \<V> x;
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e 
+    \<rbrakk> \<Longrightarrow> 
+    (\<V>, \<C>) \<Turnstile>\<^sub>e LET x = \<lparr>x\<^sub>1, x\<^sub>2\<rparr> in e
+  " |
+  Let_Left : "
+    \<lbrakk>
+      {^Left x\<^sub>p} \<subseteq> \<V> x;
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e 
+    \<rbrakk> \<Longrightarrow> 
+    (\<V>, \<C>) \<Turnstile>\<^sub>e LET x = LEFT x\<^sub>p in e
+  " |
+  Let_Right : "
+    \<lbrakk>
+      {^Right x\<^sub>p} \<subseteq> \<V> x;
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e
+    \<rbrakk> \<Longrightarrow> 
+    (\<V>, \<C>) \<Turnstile>\<^sub>e LET x = RIGHT x\<^sub>p in e
+  " |
+  Let_Send_Evt : "
+    \<lbrakk>
+      {^Send_Evt x\<^sub>c x\<^sub>m} \<subseteq> \<V> x;
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e 
+    \<rbrakk> \<Longrightarrow> 
+    (\<V>, \<C>) \<Turnstile>\<^sub>e LET x = SEND EVT x\<^sub>c x\<^sub>m in e
+  " |
+  Let_Recv_Evt : "
+    \<lbrakk>
+      {^Recv_Evt x\<^sub>c} \<subseteq> \<V> x;
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e 
+    \<rbrakk> \<Longrightarrow> 
+    (\<V>, \<C>) \<Turnstile>\<^sub>e LET x = RECV EVT x\<^sub>c in e
+  " |
+  Let_Case: "
+    \<lbrakk>
+      \<forall> x\<^sub>l' . ^Left x\<^sub>l' \<in> \<V> x\<^sub>s \<longrightarrow>
+        \<V> x\<^sub>l' \<subseteq> \<V> x\<^sub>l \<and> \<V> (\<lfloor>e\<^sub>l\<rfloor>) \<subseteq> \<V> x \<and> (\<V>, \<C>) \<Turnstile>\<^sub>e e\<^sub>l
+      ;
+      \<forall> x\<^sub>r' . ^Right x\<^sub>r' \<in> \<V> x\<^sub>s \<longrightarrow>
+        \<V> x\<^sub>r' \<subseteq> \<V> x\<^sub>r \<and> \<V> (\<lfloor>e\<^sub>r\<rfloor>) \<subseteq> \<V> x \<and> (\<V>, \<C>) \<Turnstile>\<^sub>e e\<^sub>r
+      ;
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e
+    \<rbrakk> \<Longrightarrow> 
+    (\<V>, \<C>) \<Turnstile>\<^sub>e LET x = CASE x\<^sub>s LEFT x\<^sub>l |> e\<^sub>l RIGHT x\<^sub>r |> e\<^sub>r in e
+  " |
+  Let_Fst: "
+    \<lbrakk>
+      \<forall> x\<^sub>1 x\<^sub>2. ^Pair x\<^sub>1 x\<^sub>2 \<in> \<V> x\<^sub>p \<longrightarrow> \<V> x\<^sub>1 \<subseteq> \<V> x; 
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e 
+    \<rbrakk> \<Longrightarrow> 
+    (\<V>, \<C>) \<Turnstile>\<^sub>e LET x = FST x\<^sub>p in e
+  " |
+  Let_Snd: "
+    \<lbrakk>
+      \<forall> x\<^sub>1 x\<^sub>2 . ^Pair x\<^sub>1 x\<^sub>2 \<in> \<V> x\<^sub>p \<longrightarrow> \<V> x\<^sub>2 \<subseteq> \<V> x; 
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e
+    \<rbrakk> \<Longrightarrow> 
+    (\<V>, \<C>) \<Turnstile>\<^sub>e LET x = SND x\<^sub>p in e
+  " |
+  Let_App: "
+    \<lbrakk>
+      \<forall> f' x' e' . ^Abs f' x' e' \<in> \<V> f \<longrightarrow>
+        \<V> x\<^sub>a \<subseteq> \<V> x' \<and>
+        \<V> (\<lfloor>e'\<rfloor>) \<subseteq> \<V> x
+      ;
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e
+    \<rbrakk> \<Longrightarrow> 
+    (\<V>, \<C>) \<Turnstile>\<^sub>e LET x = APP f x\<^sub>a in e
+  " |
+  Let_Sync  : "
+    \<lbrakk>
+      \<forall> x\<^sub>s\<^sub>c x\<^sub>m x\<^sub>c . 
+        ^Send_Evt x\<^sub>s\<^sub>c x\<^sub>m \<in> \<V> x\<^sub>e \<longrightarrow> 
+        ^Chan x\<^sub>c \<in> \<V> x\<^sub>s\<^sub>c \<longrightarrow>
+        {^\<lparr>\<rparr>} \<subseteq> \<V> x \<and> \<V> x\<^sub>m \<subseteq> \<C> x\<^sub>c
+      ;
+      \<forall> x\<^sub>r\<^sub>c x\<^sub>c . 
+        ^Recv_Evt x\<^sub>r\<^sub>c \<in> \<V> x\<^sub>e \<longrightarrow>
+        ^Chan x\<^sub>c \<in> \<V> x\<^sub>r\<^sub>c \<longrightarrow>
+        \<C> x\<^sub>c \<subseteq> \<V> x
+      ;
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e
+    \<rbrakk> \<Longrightarrow>  
+    (\<V>, \<C>) \<Turnstile>\<^sub>e LET x = SYNC x\<^sub>e in e
+  " |
+  Let_Chan: "
+    \<lbrakk>
+      {^Chan x} \<subseteq> \<V> x;
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e
+    \<rbrakk> \<Longrightarrow>  
+    (\<V>, \<C>) \<Turnstile>\<^sub>e LET x = CHAN \<lparr>\<rparr> in e
+  " |
+  Let_Spawn: "
+    \<lbrakk>
+      {^\<lparr>\<rparr>} \<subseteq> \<V> x;
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e\<^sub>c;
+      (\<V>, \<C>) \<Turnstile>\<^sub>e e
+    \<rbrakk> \<Longrightarrow>  
+    (\<V>, \<C>) \<Turnstile>\<^sub>e LET x = SPAWN e\<^sub>c in e
   "
+*)
+  
+(*
 
-definition state_pool_to_topo_env :: "state_pool \<Rightarrow> control_path \<Rightarrow> topo_env" ("\<langle>\<langle>_ ; _\<rangle>\<rangle>" [0, 0]61) where
-  "\<langle>\<langle>\<E> ; \<pi>\<rangle>\<rangle> = (\<lambda> x . \<langle>\<langle>\<E> ; \<pi>; x\<rangle>\<rangle>)"
+identify edges that channels can flow along (messages, sequences, calls, spawns).
 
-inductive precision_order :: "topo \<Rightarrow> topo \<Rightarrow> bool" (infix "\<preceq>" 55) where  
-  Edge1 : "OneShot \<preceq> OneToOne" | 
-  Edge2 : "OneToOne \<preceq> FanOut" |
-  Edge3 : "OneToOne \<preceq> FanIn" |
-  Edge4 : "FanOut \<preceq> ManyToMany" |
-  Edge5 : "FanIn \<preceq> ManyToMany" |
-  Refl : "t \<preceq> t" |
-  Trans : "\<lbrakk> a \<preceq> b ; b \<preceq> c \<rbrakk> \<Longrightarrow> a \<preceq> c"
+identify variables that after which channels are live
+identify variables that before which channels are live
 
-definition topo_env_precision :: "topo_env \<Rightarrow> topo_env \<Rightarrow> bool" (infix "\<sqsubseteq>\<^sub>t" 55) where
-  "\<A> \<sqsubseteq>\<^sub>t \<A>' \<equiv> (\<forall> x . \<A> x \<preceq> \<A>' x)"
+liveness trimming:
+
+if channels is not live after self application, 
+then if recursive call continuation does not contain any receives or creation of same channel
+then replace recursive call continuation with a Result
+
+*)
 
 
 definition is_static_send_path :: "(abstract_value_env \<times> abstract_value_env \<times> exp) \<Rightarrow> var \<Rightarrow> control_path \<Rightarrow> bool" where
@@ -120,6 +233,43 @@ definition static_fan_in :: "(abstract_value_env \<times> abstract_value_env \<t
   "static_fan_in \<A> x\<^sub>c \<equiv> all (is_static_recv_path \<A> x\<^sub>c) noncompetitive"
 
 
+
+(* 
+
+
+datatype topo = Non | OneShot | OneToOne | FanOut | FanIn | ManyToMany
+type_synonym topo_pair = "var \<times> topo"
+type_synonym topo_env = "var \<Rightarrow> topo"
+
+
+
+
+definition var_to_topo :: "state_pool \<Rightarrow> control_path \<Rightarrow> var \<Rightarrow> topo" ("\<langle>\<langle>_ ; _ ; _\<rangle>\<rangle>" [0,0,0]61) where
+  "\<langle>\<langle>\<E> ; \<pi>; x\<rangle>\<rangle> \<equiv>
+    (if  one_shot \<E> (Ch \<pi> x) then OneShot
+    else (if one_to_one \<E> (Ch \<pi> x) then OneToOne
+    else (if fan_out \<E> (Ch \<pi> x) then FanOut
+    else (if fan_in \<E> (Ch \<pi> x) then FanIn
+    else ManyToMany))))
+  "
+
+
+definition state_pool_to_topo_env :: "state_pool \<Rightarrow> control_path \<Rightarrow> topo_env" ("\<langle>\<langle>_ ; _\<rangle>\<rangle>" [0, 0]61) where
+  "\<langle>\<langle>\<E> ; \<pi>\<rangle>\<rangle> = (\<lambda> x . \<langle>\<langle>\<E> ; \<pi>; x\<rangle>\<rangle>)"
+
+inductive precision_order :: "topo \<Rightarrow> topo \<Rightarrow> bool" (infix "\<preceq>" 55) where  
+  Edge1 : "OneShot \<preceq> OneToOne" | 
+  Edge2 : "OneToOne \<preceq> FanOut" |
+  Edge3 : "OneToOne \<preceq> FanIn" |
+  Edge4 : "FanOut \<preceq> ManyToMany" |
+  Edge5 : "FanIn \<preceq> ManyToMany" |
+  Refl : "t \<preceq> t" |
+  Trans : "\<lbrakk> a \<preceq> b ; b \<preceq> c \<rbrakk> \<Longrightarrow> a \<preceq> c"
+
+definition topo_env_precision :: "topo_env \<Rightarrow> topo_env \<Rightarrow> bool" (infix "\<sqsubseteq>\<^sub>t" 55) where
+  "\<A> \<sqsubseteq>\<^sub>t \<A>' \<equiv> (\<forall> x . \<A> x \<preceq> \<A>' x)"
+
+
 inductive topo_pair_accept :: "topo_pair \<Rightarrow> exp \<Rightarrow> bool" (infix "\<TTurnstile>" 55) where
   OneShot: "
     \<lbrakk>
@@ -157,5 +307,7 @@ inductive topo_pair_accept :: "topo_pair \<Rightarrow> exp \<Rightarrow> bool" (
 
 definition topo_accept :: "topo_env \<Rightarrow> exp \<Rightarrow> bool" (infix "\<bind>" 55) where 
   "\<A> \<bind> e \<equiv> (\<forall> x\<^sub>c . (x\<^sub>c, \<A> x\<^sub>c) \<TTurnstile> e)"
+
+*)
 
 end
