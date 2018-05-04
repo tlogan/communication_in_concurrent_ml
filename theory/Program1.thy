8d854c1cb4e6d86042332bb3dd5d502e1654a35b
Thomas Logan <thomaslilogan@gmail.com>
2018-05-04 13:25:42 -0400

    update

--- theory/Program1.thy
+++ theory/Program1.thy
@@ -15,7 +15,7 @@ abbreviation z where "z \<equiv> Var ''z''"
 abbreviation call_server where "call_server \<equiv> Var ''call_server''"
 
 
-value "normalize (
+definition program where "program = (
 
   $LET no_chan_loop = ($FN no_chan_loop x .
     $CASE $x
@@ -25,6 +25,7 @@ value "normalize (
 
   $LET make_server = ($FN make_server x .
     $LET ch = $CHAN \<lparr>\<rparr> in
+    $LET z = $APP $no_chan_loop ($LEFT ($LEFT ($RIGHT $\<lparr>\<rparr>))) in
     $LET loop = ($FN loop x .
       $LET pair = $SYNC ($RECV EVT $ch) in
       $LET w = $FST $pair in
@@ -46,6 +47,110 @@ value "normalize (
   ) in
 
   $\<lparr>\<rparr>
+)"
+
+value "normalize program"
+
+abbreviation g100 where "g100 \<equiv> Var ''g100''"
+abbreviation g101 where "g101 \<equiv> Var ''g101''"
+abbreviation g102 where "g102 \<equiv> Var ''g102''"
+abbreviation g103 where "g103 \<equiv> Var ''g103''"
+abbreviation g104 where "g104 \<equiv> Var ''g104''"
+abbreviation g105 where "g105 \<equiv> Var ''g105''"
+abbreviation g106 where "g106 \<equiv> Var ''g106''"
+abbreviation g107 where "g107 \<equiv> Var ''g107''"
+abbreviation g108 where "g108 \<equiv> Var ''g108''"
+abbreviation g109 where "g109 \<equiv> Var ''g109''"
+abbreviation g110 where "g110 \<equiv> Var ''g110''"
+abbreviation g111 where "g111 \<equiv> Var ''g111''"
+abbreviation g112 where "g112 \<equiv> Var ''g112''"
+abbreviation g113 where "g113 \<equiv> Var ''g113''"
+abbreviation g114 where "g114 \<equiv> Var ''g114''"
+abbreviation g115 where "g115 \<equiv> Var ''g115''"
+abbreviation g116 where "g116 \<equiv> Var ''g116''"
+abbreviation g117 where "g117 \<equiv> Var ''g117''"
+abbreviation g118 where "g118 \<equiv> Var ''g118''"
+abbreviation g119 where "g119 \<equiv> Var ''g119''"
+abbreviation g120 where "g120 \<equiv> Var ''g120''"
+abbreviation g121 where "g121 \<equiv> Var ''g121''"
+abbreviation g122 where "g122 \<equiv> Var ''g122''"
+abbreviation g123 where "g123 \<equiv> Var ''g123''"
+abbreviation g124 where "g124 \<equiv> Var ''g124''"
+abbreviation g125 where "g125 \<equiv> Var ''g125''"
+abbreviation g126 where "g126 \<equiv> Var ''g126''"
+abbreviation g127 where "g127 \<equiv> Var ''g127''"
+abbreviation g128 where "g128 \<equiv> Var ''g128''"
+abbreviation g129 where "g129 \<equiv> Var ''g129''"
+abbreviation g130 where "g130 \<equiv> Var ''g130''"
+abbreviation g131 where "g131 \<equiv> Var ''g131''"
+abbreviation g132 where "g132 \<equiv> Var ''g132''"
+abbreviation g133 where "g133 \<equiv> Var ''g133''"
+abbreviation g134 where "g134 \<equiv> Var ''g134''"
+abbreviation g135 where "g135 \<equiv> Var ''g135''"
+abbreviation g136 where "g136 \<equiv> Var ''g136''"
+abbreviation g137 where "g137 \<equiv> Var ''g137''"
+abbreviation g138 where "g138 \<equiv> Var ''g138''"
+abbreviation g139 where "g139 \<equiv> Var ''g139''"
+abbreviation g140 where "g140 \<equiv> Var ''g140''"
+abbreviation g141 where "g141 \<equiv> Var ''g141''"
+abbreviation g142 where "g142 \<equiv> Var ''g142''"
+abbreviation g143 where "g143 \<equiv> Var ''g143''"
+
+
+definition anf_program where "anf_program = (
+LET g100 = FN g101 g102 . 
+
+  LET g103 = CASE g102 
+
+    LEFT g104 |> 
+      LET g105 = APP g101 g104 in 
+      RESULT g105 
+
+    RIGHT g106 |> 
+      LET g107 = \<lparr>\<rparr> in 
+      RESULT g107 
+
+  in 
+  RESULT g103 
+in 
+
+LET g108 = FN g109 g110 . 
+  LET g111 = CHAN \<lparr>\<rparr> in 
+  LET g112 = \<lparr>\<rparr> in 
+  LET g113 = RIGHT g112 in 
+  LET g114 = LEFT g113 in 
+  LET g115 = LEFT g114 in 
+  LET g116 = APP g100 g115 in 
+  LET g117 = FN g118 g119 . 
+    LET g120 = RECV EVT g111 in 
+    LET g121 = SYNC g120 in 
+    LET g122 = FST g121 in 
+    LET g123 = SND g121 in 
+    LET g124 = SEND EVT g123 g119 in 
+    LET g125 = SYNC g124 in 
+    LET g126 = APP g118 g125 in 
+    RESULT g126 
+  in 
+  LET g127 = SPAWN 
+    LET g128 = \<lparr>\<rparr> in 
+    LET g129 = RIGHT g128 in 
+    LET g130 = APP g117 g129 in 
+    RESULT g130 
+  in 
+  RESULT g111 
+in 
+LET g131 = FN g132 g133 . 
+  LET g134 = FST g133 in 
+  LET g135 = SND g133 in 
+  LET g136 = CHAN \<lparr>\<rparr> in LET g137 = \<lparr>g135, g136\<rparr> in 
+  LET g138 = SEND EVT g134 g137 in 
+  LET g139 = SYNC g138 in 
+  LET g140 = RECV EVT g136 in 
+  LET g141 = SYNC g140 in 
+  RESULT g141 
+in 
+LET g142 = \<lparr>\<rparr> in 
+RESULT g142
 )"
 
 end
